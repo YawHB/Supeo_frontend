f12550ab0fe530e00b27cd42c825c6c96473399f
YawHB <yawhboateng@gmail.com>
2025-04-16 20:57:42 +0200

    added basic useQuery to get all timeEntries on page load

--- src/components/pages/EmployeeHomePage.jsx
+++ src/components/pages/EmployeeHomePage.jsx
@@ -1,7 +1,35 @@
+import { useQuery, gql } from '@apollo/client';
+
+const GET_TIME_ENTRIES = gql`
+    query {
+        timeEntries {
+            id
+            startTime
+            endTime
+            duration
+            comment
+        }
+    }
+`;
+
 export function EmployeeHomePage() {
+    const { loading, error, data } = useQuery(GET_TIME_ENTRIES);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
     return (
         <>
             <h1>Employee Homepage</h1>
+            {data.timeEntries.map((entry) => (
+                <div key={entry.id}>
+                    <p>Starttime: {entry.startTime}</p>
+                    <p>Endtime: {entry.endtime}</p>
+                    <p>Durantion: {entry.duration}</p>
+                    <p>Comment: {entry.comment}</p>
+                    <hr />
+                </div>
+            ))}
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ApolloProvider>
     </React.StrictMode>
 );
