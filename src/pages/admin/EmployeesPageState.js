0f98fc02d674a0a27cd28f4f65a94c8a4a645a36
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-04-18 14:47:19 +0200

    add WIP page state file for employees

---
+++ src/pages/admin/EmployeesPageState.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useApolloClient } from '@apollo/client';
+
+const useEmployeesPageState = () => {
+    const apolloClient = useApolloClient();
+
+    const [ translate ] = useTranslation(`global`);
+    const [ employees, setEmployees ] = useState([]);
+    
+    const tableColumns = [
+        {
+            key: 'id',
+            label: translate('id'),
+            type: 'next',
+            sort: true,
+        },
+        {
+            key: 'firstName',
+            label: translate('first_name'),
+            type: 'text',
+            sort: true,
+        },
+        {
+            key: 'lastName',
+            label: translate('last_name'),
+            type: 'text',
+            sort: true,
+        },
+        {
+            key: 'role',
+            label: translate('role'),
+            type: 'text',
+            sort: true,
+        },
+        {
+            key: 'email',
+            label: translate('email'),
+            type: 'text',
+            sort: true,
+        },
+        {
+            key: 'phoneNumber',
+            label: translate('phone_number'),
+            type: 'text',
+            sort: true,
+        },
+    ];
+
+    return {
+        employees,
+        setEmployees,
+        tableColumns,
+        translate,
+        apolloClient,
+        useEmployeesPageState,
+    };
+}
+
+export default useEmployeesPageState;
