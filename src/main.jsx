39ebe27d8b8870cda7f461a5d0b6398d91c1a256
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-04-18 14:45:44 +0200

    add bootstrap import and i18n module for easy translations

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-import { StrictMode } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './i18n.js'
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
