44b2a667e59ac247a823482c891a71249ec85c28
YawHB <yawhboateng@gmail.com>
2025-04-18 08:20:17 +0200

    move get time entries to admin/employeeListPage

--- src/pages/employee/EmployeeHomePage.jsx
+++ src/pages/employee/EmployeeHomePage.jsx
@@ -1,36 +1,9 @@
 import { useQuery, gql } from '@apollo/client';
 
-const GET_TIME_ENTRIES = gql`
-    query {
-        timeEntries {
-            id
-            startTime
-            endTime
-            duration
-            comment
-        }
-    }
-`;
-
 export function EmployeeHomePage() {
-    console.log('enployee');
-    const { loading, error, data } = useQuery(GET_TIME_ENTRIES);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-
     return (
         <>
             <h1>Employee Homepage</h1>
-            {data.timeEntries.map((entry) => (
-                <div key={entry.id}>
-                    <p>Starttime: {entry.startTime}</p>
-                    <p>Endtime: {entry.endtime}</p>
-                    <p>Durantion: {entry.duration}</p>
-                    <p>Comment: {entry.comment}</p>
-                    <hr />
-                </div>
-            ))}
         </>
     );
 }
