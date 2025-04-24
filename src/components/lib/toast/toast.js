4322381b4bd414d12e2a2e49d56fdef967be3138
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-04-24 22:05:25 +0200

    updae toast message

--- src/components/lib/toast/toast.js
+++ src/components/lib/toast/toast.js
@@ -1,33 +1,43 @@
-// src/utils/showToast.js
 import { toast } from "react-toastify";
 
 /**
  * Show a toast with custom settings
  * @param {string} message - The message to show in the toast
- * @param {'info' | 'success' | 'error' | 'warning'} [type='info'] - Type of toast
- * @param {object} [options={}] - Optional toast settings
+ * @param {('info'|'success'|'error'|'warn')} type - Type of toast ('info', 'success', 'error', 'warn')
+ * @param {object} [options={}] - Optional configuration for the toast
  */
 const showToast = (message, type = "info", options = {}) => {
-  const toastTypeMap = {
-    success: toast.success,
-    error: toast.error,
-    warning: toast.warn,
-    info: toast.info,
-  };
-
-  const show = toastTypeMap[type] || toast.info;
-
-  show(message, {
-    position: options.position || "top-right",
-    autoClose:
-      typeof options.autoClose === "boolean"
-        ? options.autoClose
-          ? 5000
-          : false
-        : 5000,
-    className: options.wide ? "toast-wide" : "",
-    ...options,
-  });
+  switch (type) {
+    case "success":
+      toast.success(message, {
+        position: "top-right",
+        autoClose: 5000,
+        ...options,
+      });
+      break;
+    case "error":
+      toast.error(message, {
+        position: "top-right",
+        autoClose: 5000,
+        ...options,
+      });
+      break;
+    case "warn":
+      toast.warn(message, {
+        position: "top-right",
+        autoClose: 5000,
+        ...options,
+      });
+      break;
+    case "info":
+    default:
+      toast.info(message, {
+        position: "top-right",
+        autoClose: 5000,
+        ...options,
+      });
+      break;
+  }
 };
 
 export default showToast;
