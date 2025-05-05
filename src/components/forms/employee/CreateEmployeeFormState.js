66a4bc0d5b3065a3f47fe0173589e211a14d6cf3
YawHB <yawhboateng@gmail.com>
2025-05-05 12:48:49 +0200

    add employee folder for form

---
+++ src/components/forms/employee/CreateEmployeeFormState.js
@@ -0,0 +1,18 @@
+import { useInput } from '../../../hooks/useInput';
+
+const useCreateEmployeeFormState = (employee) => {
+    const input = {
+        id: useInput(employee?.id ?? null),
+        firstName: useInput(employee?.firstName ?? ''),
+        lastName: useInput(employee?.lastName ?? ''),
+        role: useInput(employee?.role ?? ''),
+        email: useInput(employee?.email ?? ''),
+        phoneNumber: useInput(employee?.phoneNumber ?? ''),
+    };
+
+    return {
+        input,
+    };
+};
+
+export default useCreateEmployeeFormState;
