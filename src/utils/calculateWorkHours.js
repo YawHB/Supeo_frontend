49ae28697263df72a64da3f696b282faf918f2a2
YawHB <yawhboateng@gmail.com>
2025-05-05 12:41:21 +0200

    show proper duration in hours and minutes in modal input

--- src/utils/calculateWorkHours.js
+++ src/utils/calculateWorkHours.js
@@ -6,13 +6,37 @@ export function calculateWorkHours(startDate, startTime, endDate, endTime) {
     //find arbejdstid
     const timeDif = msTimeEnd - msTimeStart;
     if (timeDif <= 0) {
-        return 'ugyldigt indput. start tid skal være før slut tid';
+        return 'ugyldigt input. start tid skal være før slut tid';
     }
+
+    hoursToMin(startTime, endTime);
+
     return (timeDif / 60 / 60 / 1000).toString();
 }
 
 function getMsTimeFromDate(date, time) {
     const [year, month, day] = date.split('-').map(Number);
     const [hour, min] = time.split(':').map(Number);
+
     return Date.UTC(year, month - 1, day, hour, min);
+}
+
+function hoursToMin(startTime, endTime) {
+    const [startHour, startMin] = startTime.split(':').map(Number);
+    const [endHour, endMin] = endTime.split(':').map(Number);
+    console.log(startHour, startMin);
+
+    const startTimeInMinutes = Math.floor(startHour * 60) + startMin;
+    const endTimeInMinutes = Math.floor(endHour * 60) + endMin;
+    console.log(startTimeInMinutes);
+    console.log(endTimeInMinutes);
+    const diff = endTimeInMinutes - startTimeInMinutes;
+    const hours = Math.floor(diff / 60);
+    const minutes = diff % 60;
+
+    console.log(`${hours} timer, ${minutes} minutter`);
+    // console.log(20 % 5);
+    // console.log(20 % 6);
+
+    //const timeInMinutes = Math.floor(hour * 60) + min;
 }
