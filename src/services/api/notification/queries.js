8b1381eb41f3492431997f29ff4c0f4b4c7d17ac
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-07 09:33:11 +0200

    Create queries.js

---
+++ src/services/api/notification/queries.js
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const GET_NOTIFICATION_BY_ID = gql`
+  query Notification($notificationId: ID!) {
+    notification(id: $notificationId) {
+      id
+      comment
+      status
+      timestamp
+    }
+  }
+`;
