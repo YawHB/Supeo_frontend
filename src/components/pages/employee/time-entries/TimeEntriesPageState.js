1b38afae80adf16f53aaa0ae4187a44e6ea070b3
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-07 10:38:26 +0200

    Update TimeEntriesPageState.js

--- src/components/pages/employee/time-entries/TimeEntriesPageState.js
+++ src/components/pages/employee/time-entries/TimeEntriesPageState.js
@@ -1,24 +1,41 @@
 import { useState } from "react";
-import { useQuery } from "@apollo/client";
+import { Button } from "reactstrap";
 import { useTranslation } from "react-i18next";
 import { useApolloClient } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { useModalState } from "../../../../hooks/useModalState.js";
+import { CREATE_TIME_ENTRY } from "../../../../services/api/time-entry/mutation.js";
 import { GET_TIME_ENTRIES_FOR_EMPLOYEE } from "../../../../services/api/employee/queries.js";
+
 export const useTimeEntriesPageState = () => {
   const apolloClient = useApolloClient();
-
   const [translate] = useTranslation(`global`);
   const [timeEntriesData, setTimeEntriesData] = useState([]);
+  const [isLoadingTimeEntriesForm, setIsLoadingTimeEntriesForm] = useState(false);
+  const timeEntryFormModalState = useModalState();
+  const newTimeEntryFormModalState = useModalState();
+
+  const notificationInfoModalState = useModalState();
+  const [openNotification, setOpenNotification] = useState(null);
 
   const timeEntriesColumns = [
     {
-      key: "date",
-      label: translate("date"),
+      key: "startDate",
+      label: translate("start_date"),
       type: "number",
       sort: true,
     },
     {
       key: "startTime",
       label: translate("start_time"),
+      type: "number",
+      sort: true,
+    },
+    {
+      key: "endDate",
+      label: translate("end_date"),
       type: "number",
       sort: true,
     },
@@ -35,10 +52,41 @@ export const useTimeEntriesPageState = () => {
       sort: true,
     },
     {
+      key: "break",
+      label: translate("break"),
+      type: "text",
+      sort: true,
+    },
+    {
       key: "comment",
       label: translate("comment"),
       type: "text",
       sort: true,
+    },
+    {
+      key: "status",
+      label: translate("status"),
+      type: "view",
+      options: [
+        { label: translate("pending"), value: "AFVENTER" },
+        { label: translate("approve"), value: "GODKENDT" },
+        { label: translate("reject"), value: "AFVIST" },
+        { label: translate("underway"), value: "IGANG" },
+      ],
+      alwaysEnabled: true,
+      view: (timeEntry) => (
+        <Button
+          color="primary"
+          outline
+          onClick={() => {
+            setOpenNotification(timeEntry);
+            //setOpenNotification(timeEntry.notification);
+            notificationInfoModalState.openModal();
+          }}
+        >
+          <FontAwesomeIcon icon={faCircleInfo} />
+        </Button>
+      ),
     },
     {
       key: "",
@@ -47,26 +95,49 @@ export const useTimeEntriesPageState = () => {
     },
   ];
 
-  const { loading: isLoadingTimeEntries } = useQuery(
-    GET_TIME_ENTRIES_FOR_EMPLOYEE,
-    {
-      fetchPolicy: "cache-and-network",
-      onCompleted: (data) => {
-        console.log(data);
-        setTimeEntriesData(data.employee);
+  const { loading: isLoadingTimeEntries } = useQuery(GET_TIME_ENTRIES_FOR_EMPLOYEE, {
+    fetchPolicy: "cache-and-network",
+    onCompleted: (data) => {
+      setTimeEntriesData(data.employee);
+    },
+    onError: (error) => {
+      console.error("Error fetching timeEntries:", error);
+    },
+  });
+
+  const [createTimeEntry, { loading: isSubmittingNewTimeEntry }] = useMutation(CREATE_TIME_ENTRY, {
+    refetchQueries: [GET_TIME_ENTRIES_FOR_EMPLOYEE],
+  });
+
+  const handleSubmitNewTimeEntry = (timeEntry) => {
+    setIsLoadingTimeEntriesForm(true);
+    console.log("Submitting new time entry:", timeEntry);
+
+    createTimeEntry({
+      variables: { newTimeEntry: timeEntry },
+      onCompleted: () => {
+        setIsLoadingTimeEntriesForm(false);
+        newTimeEntryFormModalState.closeModal();
+        console.log("Submitting new time entry:", timeEntry);
       },
-      onError: (error) => {
-        console.error("Error fetching timeEntries:", error);
-      },
-    }
-  );
+    });
+  };
 
   return {
     translate,
-    timeEntriesData,
-    setTimeEntriesData,
     apolloClient,
-    isLoadingTimeEntries,
+    timeEntriesData,
     timeEntriesColumns,
+    setTimeEntriesData,
+    isLoadingTimeEntries,
+    timeEntryFormModalState,
+    isLoadingTimeEntriesForm,
+    isSubmittingNewTimeEntry,
+    handleSubmitNewTimeEntry,
+    newTimeEntryFormModalState,
+    notificationInfoModalState,
+    openNotification,
+    setOpenNotification,
+    setIsLoadingTimeEntriesForm,
   };
 };
