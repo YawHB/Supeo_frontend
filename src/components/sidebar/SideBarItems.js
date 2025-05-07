b552ba5ae7e7e4f098606f559dd32b90caeebeee
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-07 10:22:13 +0200

    Update SideBarItems.js

--- src/components/sidebar/SideBarItems.js
+++ src/components/sidebar/SideBarItems.js
@@ -4,35 +4,35 @@ export const sideBarItemsMap = new Map();
 
 sideBarItemsMap.set("admin", {
   icon: faUserTie,
-  link: "/admin",
   label: "side_bar.admin_home",
+  link: "/admin",
   activeLocation: "/admin",
 });
 
 sideBarItemsMap.set("employee", {
   icon: faUser,
-  link: "/employee",
   label: "side_bar.employee_home",
+  link: "/employee",
   activeLocation: "/employee",
 });
 
 sideBarItemsMap.set("employees", {
   icon: faUsers,
-  link: "/employees",
   label: "side_bar.employees",
+  link: "/employees",
   activeLocation: "/employees",
 });
 
 sideBarItemsMap.set("timeentries", {
   icon: faClock,
-  link: "/timeentries",
   label: "side_bar.time_entries",
+  link: "/timeentries",
   activeLocation: "/timeentries",
 });
 
 sideBarItemsMap.set("employee_time_entries", {
   icon: faClock,
-  link: "/employee/time-entries",
   label: "side_bar.employee_time_entries",
+  link: "/employee/time-entries",
   activeLocation: "/employee/time-entries",
 });
