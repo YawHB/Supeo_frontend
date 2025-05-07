7333d1356fae2ab4e7db7e44191a257c744e4120
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-07 11:12:13 +0200

    Create NotificationFormState.js

---
+++ src/components/forms/time-entry/NotificationFormState.js
@@ -0,0 +1,9 @@
+import { useInput } from "../../../hooks/useInput";
+
+const useNotificationFormState = (notification = {}) => ({
+  comment: useInput(notification.comment ?? ""),
+  status: useInput(notification.status ?? ""),
+  timestamp: useInput(notification.timestamp ?? ""),
+});
+
+export default useNotificationFormState;
