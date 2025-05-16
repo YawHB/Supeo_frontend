bedcf6a90feb426b4d137a9d06b2d44c1507807d
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-16 17:39:46 +0200

    add notification form folder + corrected file path in time entries page

--- src/components/pages/employee/time-entries/TimeEntriesPage.jsx
+++ src/components/pages/employee/time-entries/TimeEntriesPage.jsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next'
 import { useTimeEntriesPageState } from './TimeEntriesPageState.js'
 import { Row, Col, Table, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 import CreateTimeEntryForm from '../../../forms/time-entry/CreateTimeEntryForm.jsx'
-import NotificationForm from '../../../forms/time-entry/NotificationForm.jsx'
+import NotificationForm from '../../../forms/notification/NotificationForm.jsx'
 
 export const EmployeeTimeEntriesPage = () => {
   const [translate] = useTranslation('global')
