2b3ff9b01d2a83e34fcdd6a23965b04a9aa58c7f
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-16 19:17:50 +0200

    update naming on routes to match names in pages folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import PageLayout from './components/PageLayout.jsx'
 import { Routes, Route } from 'react-router-dom'
+import PageLayout from './components/PageLayout.jsx'
 import ErrorBoundary from './components/error/ErrorBoundary.jsx'
 import AdminHomePage from './components/pages/admin/AdminHomePage.jsx'
-import EmployeesPage from './components/pages/admin/AdminEmployeesPage.jsx'
-import TimeEntriesPage from './components/pages/admin/AdminTimeEntriesPage.jsx'
 import EmployeeHomePage from './components/pages/employee/EmployeeHomePage.jsx'
+import AdminEmployeesPage from './components/pages/admin/AdminEmployeesPage.jsx'
+import AdminTimeEntriesPage from './components/pages/admin/AdminTimeEntriesPage.jsx'
 import { EmployeeTimeEntriesPage } from './components/pages/employee/EmployeeTimeEntriesPage.jsx'
 
 function App() {
@@ -12,14 +12,14 @@ function App() {
     <ErrorBoundary>
       <Routes>
         <Route element={<PageLayout />}>
-          <Route index element={<EmployeesPage />} />
-          <Route path='*' element={<EmployeesPage />} />
-          <Route path='/' element={<EmployeesPage />} />
+          <Route index element={<AdminEmployeesPage />} />
+          <Route path='*' element={<AdminEmployeesPage />} />
+          <Route path='/' element={<AdminEmployeesPage />} />
           <Route path='admin' element={<AdminHomePage />} />
           <Route path='employee' element={<EmployeeHomePage />} />
+          <Route path='employees' element={<AdminEmployeesPage />} />
+          <Route path='timeentries' element={<AdminTimeEntriesPage />} />
           <Route path='employee/time-entries' element={<EmployeeTimeEntriesPage />} />
-          <Route path='employees' element={<EmployeesPage />} />
-          <Route path='timeentries' element={<TimeEntriesPage />} />
         </Route>
       </Routes>
     </ErrorBoundary>
