cd0e89d43e1067a787bf7ea8f1532d285ce92aba
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-19 14:27:36 +0200

    add mutation

--- src/services/time-entry/mutations.js
+++ src/services/time-entry/mutations.js
@@ -20,3 +20,32 @@ export const CREATE_TIME_ENTRY = gql`
     }
   }
 `
+
+export const DELETE_TIME_ENTRY = gql`
+  mutation DeleteTimeEntry($id: ID!) {
+    deleteTimeEntry(id: $id)
+  }
+`
+
+export const UPDATE_TIME_ENTRY = gql`
+  mutation UpdateTimeEntry($updateTimeEntryId: ID!, $updatedTimeEntry: UpdateTimeEntryInput!) {
+    updateTimeEntry(id: $updateTimeEntryId, updatedTimeEntry: $updatedTimeEntry) {
+      id
+      startDate
+      startTime
+      endDate
+      endTime
+      duration
+      break
+      comment
+      employee {
+        id
+      }
+      notification {
+        comment
+        status
+        timestamp
+      }
+    }
+  }
+`
