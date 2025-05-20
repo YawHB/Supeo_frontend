52238ce15a54d70deebba51b2e6482da4391a648
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-20 10:42:29 +0200

    remove sidebars from pages

--- src/components/pages/admin/AdminHomePage.jsx
+++ src/components/pages/admin/AdminHomePage.jsx
@@ -1,28 +1,12 @@
 import { useTranslation } from 'react-i18next'
-import SideBar from '../../sidebar/SideBar.jsx'
-import useSideBarState from '../../sidebar/SideBarState.js'
 
 const AdminHomePage = () => {
   const [translate] = useTranslation('global')
 
   document.title = translate('page_title.admin_home')
 
-  const { sideBarItems, isSideBarCollapsed, toggleSideBarCollapse } = useSideBarState()
-
   return (
     <div className='d-flex'>
-      <div
-        style={{
-          width: isSideBarCollapsed ? '50px' : '250px',
-          transition: 'width 0.3s ease',
-        }}
-      >
-        <SideBar
-          sideBarItems={sideBarItems}
-          isSideBarCollapsed={isSideBarCollapsed}
-          toggleSideBarCollapse={toggleSideBarCollapse}
-        />
-      </div>
       <div className='flex-grow-1 p-4'>
         <h1>Admin Homepage</h1>
       </div>
--- src/components/pages/employee/EmployeeHomePage.jsx
+++ src/components/pages/employee/EmployeeHomePage.jsx
@@ -1,28 +1,12 @@
 import { useTranslation } from 'react-i18next'
-import SideBar from '../../sidebar/SideBar.jsx'
-import useSideBarState from '../../sidebar/SideBarState.js'
 
 const EmployeeHomePage = () => {
   const [translate] = useTranslation('global')
 
   document.title = translate('nav_bar.employee_home')
 
-  const { sideBarItems, isSideBarCollapsed, toggleSideBarCollapse } = useSideBarState()
-
   return (
     <div className='d-flex'>
-      <div
-        style={{
-          width: isSideBarCollapsed ? '50px' : '250px',
-          transition: 'width 0.3s ease',
-        }}
-      >
-        <SideBar
-          sideBarItems={sideBarItems}
-          isSideBarCollapsed={isSideBarCollapsed}
-          toggleSideBarCollapse={toggleSideBarCollapse}
-        />
-      </div>
       <div className='flex-grow-1 p-4'>
         <h1>Employee Homepage</h1>
       </div>
