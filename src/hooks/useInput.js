8f887d3082f76bd1d48155422286d7338a708244
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-23 16:29:47 +0200

    opdateret useInput hook

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,8 +1,18 @@
 import { useState } from 'react'
 
 export const useInput = (initialValue = '') => {
+  // const [value, setValue] = useState(initialValue)
+  // const onChange = (e) => {setValue(e.target.value)}
+
   const [value, setValue] = useState(initialValue)
-  const onChange = (e) => {setValue(e.target.value)}
+  const onChange = (eOrValue) => {
+    // håndterer både react-select og native inputs.
+    if (eOrValue && eOrValue.target !== undefined) {
+      setValue(eOrValue.target.value)
+    } else {
+      setValue(eOrValue)
+    }
+  }
 
   return {
     value,
