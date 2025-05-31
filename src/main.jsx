69ee5a74f38105beef9229c2441e5fc8d607ec17
YawHB <yawhboateng@gmail.com>
2025-05-31 18:44:45 +0200

    add headers to requests

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,24 @@ import { I18nextProvider } from 'react-i18next'
 import { BrowserRouter } from 'react-router-dom'
 import { store, persistor } from './store/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || '',
+    },
+  }
+})
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   connectToDevTools: false,
 })
