f9be6fbd82921741d7d97c6529a9b2bcecece991
YawHB <yawhboateng@gmail.com>
2025-05-31 18:44:19 +0200

    create AuthContext

---
+++ src/components/context/AuthProvider.jsx
@@ -0,0 +1,49 @@
+import { AuthContext } from './authContext.js'
+import { useState } from 'react'
+import { jwtDecode } from 'jwt-decode'
+
+/*Eksempel på decoded token
+  {
+  "user_id": 10,
+  "email": "finn_olsen@gmail.com",
+  "roleName": "Togkontrollør",
+  "permissionLevel": "Member"
+  "iat": 1746391201,
+  "exp": 1746398401
+}
+ */
+function decodeToken(token) {
+  if (token) {
+    const deccodedToken = jwtDecode(token)
+    console.log('Decoded token: ', deccodedToken)
+
+    if (deccodedToken.exp * 1000 > Date.now()) {
+      return deccodedToken
+    } else {
+      localStorage.removeItem('token')
+    }
+  }
+  return null
+}
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(() => {
+    const token = localStorage.getItem('token')
+    return decodeToken(token)
+  })
+
+  console.log('Initial user:', user)
+
+  function login(userData) {
+    localStorage.setItem('token', userData.token)
+    const decodedUser = decodeToken(userData.token)
+    setUser(decodedUser)
+  }
+
+  function logout() {
+    localStorage.removeItem('token')
+    setUser(null)
+  }
+
+  return <AuthContext.Provider value={{ user, login, logout }}>{children}</AuthContext.Provider>
+}
