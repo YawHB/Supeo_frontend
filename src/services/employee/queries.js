3669d565003c438a6d2aba1bdfdb3c767c36b117
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-31 15:27:52 +0200

    add sortinput to get all filtered employees query

--- src/services/employee/queries.js
+++ src/services/employee/queries.js
@@ -59,8 +59,8 @@ export const GET_ALL_EMPLOYEES = gql`
 `
 
 export const GET_ALL_FILTERED_EMPLOYEES = gql`
-  query GetAllEmployees($filter: EmployeeFilterInput) {
-    filteredEmployees(filter: $filter) {
+  query GetAllEmployees($filter: EmployeeFilterInput, $sort: SortInput) {
+    filteredEmployees(filter: $filter, sort: $sort) {
       id
       firstName
       lastName
