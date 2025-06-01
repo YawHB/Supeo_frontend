fea81ae1e57086ee27d92fe204a1fd3ff1d7647b
YawHB <yawhboateng@gmail.com>
2025-06-01 15:52:46 +0200

    :(

--- src/components/navbar/CurrentNavbar.jsx
+++ src/components/navbar/CurrentNavbar.jsx
@@ -1,36 +1,39 @@
-import { Row, Col } from 'reactstrap'
-import { NavLink } from 'react-router-dom'
+// import { Row, Col } from 'reactstrap'
+// import { NavLink } from 'react-router-dom'
 
-export default function NavBar() {
-  return (
-    <div>
-      <Row>
-        <Col>
-          <NavLink className='NavLink' to='/admin/home'>
-            Admin - Homepage
-          </NavLink>
-        </Col>
-        <NavLink className='NavLink' to='/admin/employees'>
-          Admin - Employees Page
-        </NavLink>
-        <NavLink className='NavLink' to='/admin/timeentries'>
-          Admin - Time Entries Page
-        </NavLink>
-      </Row>
-      <Row>
-        <Col>
-          <NavLink className='NavLink' to='/employee/home'>
-            Employee - Homepage
-          </NavLink>
-        </Col>
-        <Row>
-          <Col>
-            <NavLink className='NavLink' to='/employee/time-entries'>
-              Employee - Time entries page
-            </NavLink>
-          </Col>
-        </Row>
-      </Row>
-    </div>
-  )
-}
+// export default function NavBar() {
+//   return (
+//     <div>
+//       {/* {
+//             user.permissionLevel === 'Admin' || user.permissionLevel === 'Manager' && */}
+//       <Row>
+//         <Col>
+//           <NavLink className='NavLink' to='/admin/home'>
+//             Admin bør ikke have en admin homepage, da han allerede har "min profil"
+//           </NavLink>
+//         </Col>
+//         <NavLink className='NavLink' to='/admin/employees'>
+//           Ansatte
+//         </NavLink>
+//         <NavLink className='NavLink' to='/admin/timeentries'>
+//           Vagtoversigt
+//         </NavLink>
+//       </Row>
+//       {/* } */}
+//       <Row>
+//         <Col>
+//           <NavLink className='NavLink' to='/employee/home'>
+//             Min profil
+//           </NavLink>
+//         </Col>
+//         <Row>
+//           <Col>
+//             <NavLink className='NavLink' to='/employee/time-entries'>
+//               Mine vagter
+//             </NavLink>
+//           </Col>
+//         </Row>
+//       </Row>
+//     </div>
+//   )
+// }
