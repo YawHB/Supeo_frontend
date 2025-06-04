855a3d2af4c35ef94e21b6981afe9752d447ad06
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-06-05 00:22:50 +0200

    add role based home page based on the users permission

--- src/components/pages/admin/AdminHomePage.jsx
+++ src/components/pages/admin/AdminHomePage.jsx
@@ -1,13 +1,18 @@
 import { useTranslation } from 'react-i18next'
+import { useContext } from 'react'
+import { AuthContext } from '../../context/authContext.js'
 
 const AdminHomePage = () => {
   const [translate] = useTranslation('global')
+  const { user } = useContext(AuthContext)
 
   document.title = translate('page_title.admin_home')
 
   return (
     <div className='d-flex'>
-      <h1>Admin Homepage</h1>
+      <h1>
+        {translate('login.welcome')} {user.email}
+      </h1>
     </div>
   )
 }
